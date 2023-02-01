94af45aed9e88e0d77040d33f069a3d0dd25fece
Alexmatthew Candelario <alex@candtechsoftware.com>
2023-02-01 15:25:43 -0500

    added main file

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Cool New Project');
+
+function addition(a,b) {
+  return a + b;
+}
+
+
+function main() {
+  console.log(addition(1,2));
+  console.log(addition(1,2));
+  console.log(addition(1,2));
+  console.log(addition(1,2));
+
+}
+
+
+main()

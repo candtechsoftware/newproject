148eda29051cf92ebcce72bbd3613a1dc9eee9b5
Alexmatthew Candelario <alex@candtechsoftware.com>
2023-02-01 15:31:23 -0500

    created multipy function

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ function addition(a,b) {
   return a + b;
 }
 
+function multiply(a,b) {
+}
 
 function main() {
   console.log(addition(1,2));
   console.log(addition(1,2));
-  console.log(addition(1,2));
+  console.log(multiply(1,2));
   console.log(addition(1,2));
 
 }
